4bce1f9d89e55171c87d0d6bc67133ef16c4633f
filipnaumovic <filip@salsitasoft.com>
2014-12-31 20:54:42 +0100

    timeline script, anim draft, audio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,23 @@
 $(function() {
-  var grid = $('.grid');
 
-  var tween = TweenMax.to(grid, 3, {rotationX:"75deg", height: "50%", scale: 1, opacity: 1, ease:Back.easeInOut, delay: 5});
+  // Elements
+  var bgSound = $('#bgsound');  
+  var elGrid = $('#grid');
+  var elMsg1 = $('#msg-1');
+  var elMsg2 = $('#msg-2');
+  var elMsg3 = $('#msg-3');
+  var elLogo = $('#logo');
+
+  // Timeline script
+  var flushCapacitorIntro = new TimelineMax();  
+  
+  flushCapacitorIntro
+      .set(elGrid, {opacity: 0.1})
+      .to(elMsg1, 0.5, {autoAlpha: 1, onComplete: bgSound.get(0).play()})
+      .to(elMsg1, 0.5, {autoAlpha: 0}, "+=5")
+      .to(elMsg2, 0.5, {autoAlpha: 1}, "+=1")
+      .to(elMsg2, 0.5, {autoAlpha: 0}, "+=5")
+      .to(elGrid, 3, {rotationX:"75deg", height: "50%", bottom: "-100px", scale: 1,  opacity: 1, ease:Back.easeInOut})
+      .to(elLogo, 0.5, {autoAlpha: 1, y: "-=100px", scale: 1.1}, "-=.7");
 
 });
