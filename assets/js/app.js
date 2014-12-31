8ded02e9ecc1e1725b2a9e76397b214792c2c2fd
filipnaumovic <filip@salsitasoft.com>
2014-12-31 21:56:04 +0100

    video, adjusted script

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,19 +5,21 @@ $(function() {
   var elGrid = $('#grid');
   var elMsg1 = $('#msg-1');
   var elMsg2 = $('#msg-2');
-  var elMsg3 = $('#msg-3');
   var elLogo = $('#logo');
+  var elVideo = $('#lights');
 
-  // Timeline script
-  var flushCapacitorIntro = new TimelineMax();  
+  // Main timeline script
+  var flushCapacitorTimeline = new TimelineMax();  
   
-  flushCapacitorIntro
+  flushCapacitorTimeline
       .set(elGrid, {opacity: 0.1})
       .to(elMsg1, 0.5, {autoAlpha: 1, onComplete: bgSound.get(0).play()})
       .to(elMsg1, 0.5, {autoAlpha: 0}, "+=5")
       .to(elMsg2, 0.5, {autoAlpha: 1}, "+=1")
       .to(elMsg2, 0.5, {autoAlpha: 0}, "+=5")
-      .to(elGrid, 3, {rotationX:"75deg", height: "50%", bottom: "-100px", scale: 1,  opacity: 1, ease:Back.easeInOut})
-      .to(elLogo, 0.5, {autoAlpha: 1, y: "-=100px", scale: 1.1}, "-=.7");
-
+      .to(elGrid, 3, {rotationX:"75deg", height: "60%", bottom: "-100px", scale: 1,  opacity: 1, ease:Back.easeInOut})
+      .to(elVideo, 0.5, {autoAlpha: 0.6}, "-=.8")
+      .to(elLogo, 0.5, {autoAlpha: 1, y: "-=100px", scale: 1.1}, "-=.8");
+  
+    
 });
