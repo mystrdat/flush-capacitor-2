dcdd5459b2795333513f2b52eac5ee2a5cd9aeba
filipnaumovic <filip@salsitasoft.com>
2015-01-01 02:19:35 +0100

    improved logo entrance and text fade

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,11 +18,11 @@ $(function() {
       .to(badParts, 0.5, {autoAlpha: 1, onComplete: bgSound.get(0).play()}, "start")
       .to(badParts, 0.5, {autoAlpha: 0}, "+=2")
       .to(hopeHasRisen, 0.5, {autoAlpha: 1}, "+=1")
-      .to(hopeHasRisen.find('disappear-early'), 0.5, {autoAlpha: 0}, "+=4")
-      .to(hopeHasRisen, 0.5, {autoAlpha: 0}, "+=1")
+      .to(hopeHasRisen.find('disappear-early'), 1, {autoAlpha: 0, ease: Bounce.easeInOut}, "+=4")
+      .to(hopeHasRisen, 0.5, {autoAlpha: 0}, "+=0.5")
       .to(fakeMesh, 3, {rotationX: "75deg", height: "60%", bottom: "-100px", scale: 1,  opacity: 1, ease:Back.easeInOut}, "-=.5")
       .to(lights, 0.5, {autoAlpha: 0.8, scale: 6}, "-=.8")
-      .to(flushCapacitor, 0.5, {autoAlpha: 1, 'margin-top': "-=100px", scale: 1.3}, "-=.8");
+      .to(flushCapacitor, 0.5, {autoAlpha: 1, 'margin-top': "-=100px", scale: 1.3, ease: Strong.easeOut}, "-=.8");
     
   // Mute toggle
   
